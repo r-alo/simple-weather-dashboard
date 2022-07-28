3edf2d3b802194ba9d597b8af62b472feb0f6762
r-alo <email@example.com>
2022-07-28 14:46:27 -0500

    created HTML and responsive with Bootstrap / create current Weahter API fetch and html placement

---
+++ js/index.js
@@ -0,0 +1,59 @@
+//API key variable
+const apiKey = '2d5cb8ee08306bcd2e221943675f5ba4';
+
+// HTML get Element variables
+    //Input search variables
+const citySearch = document.getElementById('city-search');
+let storedCities = document.getElementById('stored-cities');
+
+// Current weather API call
+let currentWeather = {
+    getWeather: function(city) {
+        fetch(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + apiKey
+        )
+        .then(response => response.json())
+        .then(data => 
+            console.log(data) &
+            this.displayCurrent(data)
+        );
+    },
+
+    //Get main current weather and display in HTML
+    displayCurrent: function(data) {
+        const {name} = data;    
+        const {icon} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind ;
+        const {lat, lon} = data.coord;
+        console.log(name, icon, temp, speed, humidity);
+        //Display data in respective HTML tags
+        document.querySelector('#main-city').innerText = name,
+        document.querySelector('#main-icon').src = "http://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector('#main-temp').innerText = 'Temperature: ' + temp + ' C°',
+        document.querySelector('#main-wind').innerText = 'Wind: ' + speed + ' KPH',
+        document.querySelector('#main-humid').innerText = 'Humidity: ' + humidity + ' %';
+        this.getUV(lat, lon);
+    },
+
+    //Get UV Index via another fetch for HTML display
+    getUV: function (lat, lon) {
+        fetch(
+            'https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,daily,hourly,alerts&units=metric&appid=' + apiKey
+        )
+            .then(response => response.json())
+            .then(data =>
+                console.log(data) &
+                this.displayUV(data)
+            );
+    },
+    displayUV: function (data) {
+        const { uvi } = data.current;
+        console.log(uvi);
+        document.querySelector('#main-uvi').innerText = 'UV Index: ' + uvi;
+    }
+
+}
+
+
+
